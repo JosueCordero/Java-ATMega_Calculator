eb8a054ebaad9d5f31b1e4854975ef598d643c14
Josue Cordero <josuecor06@gmail.com>
2022-06-08 01:51:00 -0500

    First commit

---
+++ Calculadora_v_commSerial/src/calculadora_v_commserial/Calculadora_v_commSerial.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculadora_v_commserial;
+
+/**
+ *
+ * @author Josue Cordero
+ */
+public class Calculadora_v_commSerial {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
